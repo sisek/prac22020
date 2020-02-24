d4b193c874efeae7957e2597576083798a921e43
sisek <sisekopeter98@gmail.com>
2020-02-24 16:09:18 +0200

    added all files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Problems
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        public string Prefix(string input){
+
+            if (input == null)
+                return null;
+            
+            //get the length
+            int length = input.Length;
+
+            //get the number of words, ignoring empty entries
+             string[] splitInput = input.Split(" ");
+
+            int words = 0;
+            for(int i = 0; i < splitInput.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(splitInput[i]))
+                    words++;
+            }    
+
+            //combine into one output
+            string output = length + "," + words + ":" + input;
+
+            //return output
+            return output;
+        }
+
     }
+
 }
